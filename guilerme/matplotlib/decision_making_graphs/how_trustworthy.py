d814b7ae76f3f85f93b5e8d261e2bd0319399a1c
guinoronhaf <guilhermenoronhaf@gmail.com>
2026-02-10 14:55:34 -0300

    feat: adding error bars

--- guilerme/matplotlib/decision_making_graphs/how_trustworthy.py
+++ guilerme/matplotlib/decision_making_graphs/how_trustworthy.py
@@ -11,7 +11,12 @@ values = [(float((df[df['ia_confiavel'] == n]).nunique()['data_hora']/total_answ
 fig, ax = plt.subplots()
 rects = ax.bar(categories, values, color='1', edgecolor='0', width=0.5, linewidth=1.5)
 
+x = range(0, 11)
+y = values
+
 ax.bar_label(rects, padding=3, fmt='%.1f')
+
+ax.errorbar(x, y, yerr=2, fmt='o', color='0')
 
 ax.set_xticks(range(len(categories)))
 ax.set_ylabel('%')
